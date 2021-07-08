76c16d4289a80f359791902a2e20a5f47991f31a
AkshayKumarTN <tnak369@gmail.com>
2021-07-08 08:50:45 +0530

    [Akshay] initial Commit

---
+++ Generics Test Maximum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_Test_Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics Test Maximum");
+        }
+    }
+}
